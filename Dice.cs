939bd3faa5aafbf1d0be78457606d3478a926a95
aruru36 <60528720+aruru36@users.noreply.github.com>
2020-10-19 15:29:25 +0900

    Add files via upload

---
+++ Dice.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dice : MonoBehaviour
+{
+    public Image img;
+    public Sprite[] images;
+
+    public void ButtonDown()
+    {
+        int num = Random.Range(0, images.Length);
+        img.sprite = images[num];
+    }
+}
